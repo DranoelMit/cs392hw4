b49a2dce1294b71b9ef3fd850fdf8efca114e5d3
DranoelMit <tim.matthew.leonard@gmail.com>
2018-11-14 21:55:45 -0500

    Add transpose function(empty)

--- transp.c
+++ transp.c
@@ -10,19 +10,31 @@ void fillMatrix(float *arr, int size){
 	
 }
 
+int transpose(float *input, float *output, int N, int blockSize){
+		
+	//row i column j input[i*N + j]
+	return 0;
+}
+
 
 
 int main(int argc, char *argv[]){
 
 	if(argc!=3){
 	printf("Invalid number of arguments\n Proper format: transp <matrix width> <block width>\n");
-	exit(1);	
+	return -1;	
 	}
-
+	//both argv[1] and argv[2] should be powers of 2
 	int matrixSize = atoi(argv[1])  * atoi(argv [1]);
 	float *input = malloc(matrixSize * sizeof(float));
 	float *output = malloc(matrixSize * sizeof(float));
 	
 	fillMatrix(input, matrixSize);
-	exit(0);
+
+	if(transpose(input, output, atoi(argv[1]), atoi(argv[2])) < 0){
+		printf("error");
+		return -1;
+	}
+
+	return 0;
 }
